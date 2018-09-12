03db9dfd1d34bb34eaf02428e07ad42c06dfaf6a
Rashida Francis <rashida_francis@m018130.aw.org>
2018-09-12 14:33:02 -0700

    a message

--- AboutMe.java
+++ AboutMe.java
@@ -1,17 +1,17 @@
 //AboutMe.java by Rashida
-// 9-10-18
+// 9-12-18
 
 import java.util.*;
 
 public class AboutMe{
 
   public String color;// creation of the static variable
-  public static String hand;
-  public static String subject;
-  public String interest;
+  public static String hand; // right/left hand
+  public static String subject;// this means favorite subject
+  public String interest; // as in interest that you have but others may find it boring
   public static String birthplace;
-  public static Integer siblings;
-  public static String time;
+  public static Integer siblings; // as in how many
+  public static String time; // as in morning/night person
 
 
 public AboutMe() {
@@ -23,12 +23,38 @@ public AboutMe() {
   siblings = 4;
   time = "morning";
 }
-/*
-public static void main (String)[] args) {
 
-  AboutMe Rashida = new AboutMe(); // call constructor method
-  system.out.println("Well"+ Rashida.name);
+public static void main ( String [] args){
 
+  AboutMe Anna = new AboutMe(); // call constructor method
+
+    System.out.println ("You will learn about Anna"); // a greeting
+    System.out.println ("Anna's favorite color is " + Anna.color);
+    System.out.println ("Anna is " + Anna.hand);
+    System.out.println ("Anna's favorite color is " + Anna.subject);
+    System.out.println ("Anna's interest is " + Anna.interest);
+    System.out.println ("Anna was born in " + Anna.birthplace);
+    System.out.println ("Anna has " + Anna.siblings);
+    System.out.println ("Anna is a " + Anna.time);
+
+    Scanner colorInp = new Scanner(System.in); //scanner for mutable variable
+      String colorIn = colorInp.next();
+      if (colorIn.equals("a")) {
+        System.out.println(Anna.color);
+      }
+      else if (colorIn.equals("b")) {
+        System.out.println(" Yellow for the Sun");
+      }
+    Scanner interestInp = new Scanner(System.in); // scanner for mutable variable
+     String interestIn = interestInp.next ();
+     if (interestIn.equals("a")) {
+       System.out.println(Anna.interest);
+     }
+     else if (interestIn.equals("b")) {
+       System.out.println (" riding a bike");
+     }
  }
-*/
+
+
+
 }
