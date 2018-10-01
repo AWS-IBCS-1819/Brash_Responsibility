3a844148f56499e1ddd408461b0ce6bd419271ca
Rashida Francis <rashida_francis@m018130.aw.org>
2018-10-01 14:11:16 -0700

    a loop

---
+++ loopExample.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class loopExample {
+
+ public static void main (String [] args ) {
+
+// Example #2
+int sum = 0;
+int x = 15;
+int y = 39;
+for (int i = x; i < y + 1; i++) {
+  sum = sum + i;
+ }
+ System.out.println (sum);
+
+ // Example #5
+x = 18;
+long product = 1;
+ for (int i = 1; i <= x; i++) {
+   product = product * i;
+ }
+ System.out.println (product);
+
+ }
+}
