4906ea273783384fbc047a4d66e56c9480087c3a
Rashida Francis <rashida_francis@m018130.aw.org>
2018-10-05 13:48:23 -0700

    a Deck

---
+++ Deck.java
@@ -0,0 +1,32 @@
+
+
+
+import java.util.*;
+
+public class Deck {
+
+  ArrayList<String> card;
+
+  public Deck(){
+    card = new ArrayList<String>();
+
+
+    for (int i = 1; i< 14; i++){
+      String num = Integer.toString(i);
+      card.add(i + "of Diamonds");
+      }
+    for (int j = 14; j < 27; j++){
+      String num= Integer.toString (j);
+      card.add("of Clubs");
+    }
+    for (int k= 27; k < 40; k++){
+      String num = Integer.toString (k);
+      card.add("of Hearts");
+
+    }
+    for (int l = 40; l < 53; l++){
+      String num = Integer.toString (l);
+      card.add("of Spades");
+    }
+  }
+}
