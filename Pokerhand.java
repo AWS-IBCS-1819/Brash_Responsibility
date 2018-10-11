d9b40ef817499f2ad2ca0d5746fc0ff545dce219
Rashida Francis <rashida_francis@m018130.aw.org>
2018-10-11 12:53:31 -0700

    Pokerhand

--- Pokerhand.java
+++ Pokerhand.java
@@ -1,11 +1,24 @@
+//Pokerhan.java by Rashida
+
+import java.util.*;
 
 public class Pokerhand {
 
-public static void main (String [] args) {
+  public Deck shuffle(Deck d){
+      return d;
+    }
 
-Deck d = new Deck();
+    public static void main(String[] args) {
+      Deck d = new Deck();
 
-System.out.println (d.card);
+      Random r = new Random(); //<-- this is to create a new object; constructor method
+      int[] ar = r.ints(6300, 0, 52).distinct().toArray();//can't just print array
+      for (int i = 0; i < ar.length; i ++){//loop is to help print contents of array
+        //System.out.println(ar[i]);
+        System.out.println(d.cards.get(ar[i]));
+      }
+
+
 
 }
 
