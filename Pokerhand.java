154cfdb80285b22d3bd1bbff3aa5187be87eaab8
Rashida Francis <rashida_francis@m018130.local>
2018-10-15 17:25:10 -0700

    Pokerhand

--- Pokerhand.java
+++ Pokerhand.java
@@ -1,25 +1,82 @@
-//Pokerhan.java by Rashida
+//Pokerhand.java 
 
 import java.util.*;
 
 public class Pokerhand {
 
-  public Deck shuffle(Deck d){
-      return d;
+  Deck d;
+
+  public Deck shuffle(Deck d) {
+    Random r = new Random();
+    ArrayList<String> shuffled = new ArrayList<String>();
+
+    int[] ar = r.ints(5200, 0, 52).distinct().toArray();
+    for (int i = 0; i < 52; i ++) {
+      shuffled.add(d.cards.get(ar[i]));
+    }
+    d.cards = shuffled;
+    return d;
+  }
+
+  public ArrayList<String> giveHand(Deck d) {
+
+    ArrayList<String> hand = new ArrayList<String>(5);
+    for (int i = 0; i < 5; i++) {
+      hand.add(d.cards.get(i));
+    }
+    return hand;
+  }
+
+  public String evaluate(ArrayList<String> hand) {
+    String result = "";
+    int numD = 0;
+    int numC = 0;
+    int numH = 0;
+    int numS = 0;
+
+    for (int i = 0; i < 5; i++) {
+      if (hand.get(i).contains("Diamonds")) {
+        numD++;
+      }
+      else if (hand.get(i).contains("Clubs")) {
+        numC++;
+      }
+      else if (hand.get(i).contains("Hearts")) {
+        numH++;
+      }
+      else {
+        numS++;
+      }
     }
 
-    public static void main(String[] args) {
-      Deck d = new Deck();
+    if (numD == 5 || numC == 5 || numH == 5 || numS == 5) {
+      result = "FLUSH";
+      System.out.println(result);
+      System.out.println(hand);
+    }
+    return result;
+  }
 
-      Random r = new Random(); //<-- this is to create a new object; constructor method
-      int[] ar = r.ints(6300, 0, 52).distinct().toArray();//can't just print array
-      for (int i = 0; i < ar.length; i ++){//loop is to help print contents of array
-        //System.out.println(ar[i]);
-        System.out.println(d.cards.get(ar[i]));
-      }
+  public Pokerhand() {
+    d = new Deck();
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Number of hands dealt: 1");
+
+    Pokerhand p = new Pokerhand();
+    //System.out.println(p.d.cards);
+    for (int i = 0; i < 1; i++) {
+      p.d = p.shuffle(p.d);
+      ArrayList<String> h = p.giveHand(p.d);
+      System.out.println(h);
+      String res = p.evaluate(h);
+    }
+
+    //System.out.println(h);
 
 
 
-}
+  }
 
 }
