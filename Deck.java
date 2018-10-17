2d5095a026e03d99eb696a1ddae1767e461c6d76
Rashida Francis <rashida_francis@m018130.aw.org>
2018-10-17 12:58:58 -0700

    Deck

--- Deck.java
+++ Deck.java
@@ -1,14 +1,40 @@
-//Deck.java 
+//Deck.java by Rashida
 //10/5/18
 
 import java.util.*;
 
-public class Deck {
+public class Deck { // create class
 
-  ArrayList<Card> cards;
+  ArrayList<Card> cards; // ArrayList of card objects - main deck
+  ArrayList<Card> discard; // ArrayList of discarded cards - keep track of what is given out/ the cards that has been handed out
 
-  public Deck() {
+  public Card drawCard() { // this method draws a card
+    Card c = cards.get(0);
+    cards.remove (0); // remove the card from the deck
+    discard.add (c); // add the removed card to the discarded pile- count as one we gave out
+    return c; // the method returns a card object
+  }
+
+  public void reset () { // adds everything in discard back into the main deck
+    cards.addAll (discard);
+    discard.clear (); // get rid of what discard is
+  }
+
+  public void shuffle () {  // mixing up the deck of cards
+    Random r = new Random (); // using a random object because shuffling is random
+    ArrayList<Card> shuffled = new ArrayList<Card> ();
+
+    int [] ar = r.ints (10000, 0, 52).distinct ().toArray (); // creates an integer Array of all of the ditinct integer giving it a integer stream
+    for (int i = 0; i < 52; i++) {
+      shuffled.add(cards.get(ar[i]));
+    }
+
+    cards = shuffled;
+
+  }
+   public Deck () {
     cards = new ArrayList<Card>();
+    discard = new ArrayList<Card>();
 
     for (int i = 1; i <= 13; i++) {
       for (int j = 1; j <= 4; j++) {
@@ -44,7 +70,8 @@ public class Deck {
         Card c = new Card ();
         cards.add(c);
         c.setValue(i);
-        cards.setName(num + "of" + suit);
+        c.setName(num + "of" + suit);
       }
     }
   }
+}
