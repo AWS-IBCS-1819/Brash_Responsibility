b487c8304b8a342c3b63f5a05d888f30786ec611
Rashida Francis <rashida_francis@m018130.aw.org>
2018-11-09 12:29:09 -0800

    Enclosure

---
+++ Enclosure.java
@@ -0,0 +1,22 @@
+//Enclosure.java by Julia and Rashida
+
+public class Enclosure{
+   String type;
+  int temp;
+   public void setType(String ty) {
+    type = ty;
+  }
+   public String getType() {
+    return type;
+  }
+   public void setTemp(int t) {
+    temp = t;
+  }
+   public Integer getTemp() {
+    return temp;
+  }
+   public Enclosure() {
+    type = "";
+    temp = 0;
+  }
+}
