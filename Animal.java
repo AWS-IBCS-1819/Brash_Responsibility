9c1a11b5919783f6ed955616eff0cdfe09f61a22
Rashida Francis <rashida_francis@m018130.local>
2018-11-14 22:02:27 -0800

    Animal

--- Animal.java
+++ Animal.java
@@ -1,14 +1,40 @@
 // Animal.java by Julia and Rashida
 
 public class Animal{ //creating a class for all the animals
-   String name; //create a string for animal names
-   public void setName(String n) { //method for setting names
+
+  String name; //create a string for animal names
+  String color; //create a string for color
+  int height; //create a integer for height
+
+  public void setName(String n) { //method for setting names
     name = n;
   }
-   public String getName() { //method for getting names
+
+  public String getName() { //method for getting names
     return name;
   }
-   public Animal() {
-    name = "";
+
+  public void setColor(String c) { //method for setting color
+    color = c;
   }
- }
+
+  public String getColor() { //method for getting color
+    return color;
+  }
+
+  public void setHeight(int h) { //method for setting height
+    height = h;
+  }
+
+  public int getHeight() { //method for getting height
+    return height;
+  }
+
+
+  public Animal(String n, String c, int h) {
+    this.name = n;
+    this.color = c;
+    this.height = h;
+  }
+
+}
