4fa942ace15abbff70f03099f0d4fdfee20da3bf
Rashida Francis <rashida_francis@m018130.aw.org>
2019-02-01 13:52:11 -0800

    Amphibia

---
+++ Amphibia.java
@@ -0,0 +1,33 @@
+
+
+public class Amphibia {
+
+ public String thinskin;
+ public String glandular;
+ public String magid;
+ public String tetrapods;
+
+ public Amphibia (String startThinskin, String startGlandular, String startMagid, String startTetrapods) {
+   thinskin = startThinskin;
+   glandular = startGlandular;
+   magid = startMagid;
+   tetrapods = startTetrapods;
+ }
+
+ public void setThinskin (String newName) {
+   thinskin = newName;
+ }
+
+ public void setGlandular (String newName) {
+   glandular = newName;
+ }
+
+ public void setMagid (String newName)  {
+   magid = newName;
+ }
+
+ public void setTetratpods (String newName) {
+   tetrapods = newName;
+ }
+
+}
